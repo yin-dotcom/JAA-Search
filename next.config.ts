e6a17947296d076e5ab6ee75b9fe61caa16694b9
yin-dotcom <yin@caro-recycle.com>
2026-07-14 15:25:22 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // 警告：让 Vercel 忽略 ESLint 语法检查，强制打包
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // 警告：让 Vercel 忽略 TypeScript 类型报错，强制打包
     ignoreBuildErrors: true,
